38c618137c74fc71731dd683b0546345cdfbbd82
Grigoriy Dobryakov <grigoriydobryakov@gmail.com>
2015-08-28 20:19:58 +0300

    logentries

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :production do
 
 gem 'pg'
 gem 'rails_12factor'
+gem 'le'
 
 end
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger = Le.new('57d8719e-9a6c-4c1c-9351-5b857a2991b5') if Rails.env.production?
